cd2517b17f893ae307e7d2be021820c1cc1d0a39
RizkinaFatikhatusS <165800759+RizkinaFatkhatusS@users.noreply.github.com>
2025-06-03 12:36:46 +0700

    Add files via upload

---
+++ Tugas_Mingguan/Tugas 8/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 8</title>
+    <style>
+        fieldset {
+            width: 350px;
+            padding: 15px;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <form action="latihan_nilai.php" method="get">
+        Nama: <input type="text" name="nama"><br>
+        Nilai : <input type="text" name="nilai"><br>
+        <input type = "submit" value="proses">
+    </form>
+</body>
+</html>
